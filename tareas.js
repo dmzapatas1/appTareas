5cd821c5ccba7d80eb4a24a6c81f974b6b5e26f4
dmzapatas1 <dmzapatas10@gmail.com>
2021-09-21 13:05:50 -0500

    Primer commit del app de tareas

---
+++ tareas.js
@@ -0,0 +1,33 @@
+let fs= require('fs')
+const tareas = require('./tareas.json');
+
+const Listar = () => {
+    tareas.forEach(function(tarea){
+        console.log( tarea.titulo +"," +tarea.estado )
+    });
+}
+
+const EscribirjSON = (array) =>{
+    const arrayJSON = JSON.stringify(array)
+    fs.writeFileSync('./tareas.json',arrayJSON,null,4)
+}
+const LeerjSON = ()=> {
+    return JSON.parse(fs.readFileSync('./tareas.json'));
+}
+const GuardarTarea = (titulo, estado,)=>{
+    let db = LeerjSON()
+    const tareaNueva ={
+        titulo : titulo,
+        estado : estado,
+    };
+    db.push(tareaNueva);
+    EscribirjSON(db);
+}
+
+const FiltrarPorEstado = (estado)=>{
+    let bd = LeerjSON();
+    let filtrada = bd.filter(item => item.estado == estado)
+    filtrada.forEach(filtrada => console.log( filtrada.titulo +"," +filtrada.estado ));
+}
+
+module.exports = {Listar,EscribirjSON, GuardarTarea, FiltrarPorEstado}
